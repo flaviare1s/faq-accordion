69377fca7f9a823dec6ab1f8ac14888b33aea9cc
Flávia Reis <flaviarster@gmail.com>
2024-01-23 13:24:02 -0300

    Update index, style and script

--- script.js
+++
@@ -1,9 +0,0 @@
-function showAnswer() {
-    const questButtonEls = document.querySelectorAll('.questButton')
-
-    questButtonEls.forEach((questButtonEl) => {
-        questButtonEl.addEventListener('click', () => alert('teste'))
-    })
-}
-
-showAnswer()
