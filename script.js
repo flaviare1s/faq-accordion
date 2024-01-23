915f67a31941b5b2a3927ef7f0fc3b10dd133d8f
Flávia Reis <flaviarster@gmail.com>
2024-01-23 14:23:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,68 @@ function showAnswer() {
         }
     })
 
+    const questButton2El = document.querySelector('#questButton2')
+    const answer2El = document.querySelector('#answer2')
+    const minus2El = document.querySelector('#minus2')
+    const plus2El = document.querySelector('#plus2')
+
+    questButton2El.addEventListener('click', () => {
+        if (answer2El.style.display === 'block') {
+            answer2El.style.display = 'none' 
+        } else {
+            answer2El.style.display = 'block' 
+        }
+        
+        if (minus2El.style.display === 'block') {
+            minus2El.style.display = 'none'
+            plus2El.style.display = 'block'
+        } else {
+            minus2El.style.display = 'block'
+            plus2El.style.display = 'none'
+        }
+    })
+
+    const questButton3El = document.querySelector('#questButton3')
+    const answer3El = document.querySelector('#answer3')
+    const minus3El = document.querySelector('#minus3')
+    const plus3El = document.querySelector('#plus3')
+
+    questButton3El.addEventListener('click', () => {
+        if (answer3El.style.display === 'block') {
+            answer3El.style.display = 'none' 
+        } else {
+            answer3El.style.display = 'block' 
+        }
+        
+        if (minus3El.style.display === 'block') {
+            minus3El.style.display = 'none'
+            plus3El.style.display = 'block'
+        } else {
+            minus3El.style.display = 'block'
+            plus3El.style.display = 'none'
+        }
+    })
+
+    const questButton4El = document.querySelector('#questButton4')
+    const answer4El = document.querySelector('#answer4')
+    const minus4El = document.querySelector('#minus4')
+    const plus4El = document.querySelector('#plus4')
+
+    questButton4El.addEventListener('click', () => {
+        if (answer4El.style.display === 'block') {
+            answer4El.style.display = 'none' 
+        } else {
+            answer4El.style.display = 'block' 
+        }
+        
+        if (minus4El.style.display === 'block') {
+            minus4El.style.display = 'none'
+            plus4El.style.display = 'block'
+        } else {
+            minus4El.style.display = 'block'
+            plus4El.style.display = 'none'
+        }
+    })
 }
 
 showAnswer()
